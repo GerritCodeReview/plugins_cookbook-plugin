1f074e72bffb29c77ab6bed14373f17354e21b33
David Ostrovsky <david@ostrovsky.org>
2013-08-18 10:01:11 +0200

    Add hello UiAction
    Add UiAction "Say hello" for RevisionResouce. It works on both old and new
    change screen. User has to be logged in to see the buttton. Alert window
    is shown with greeting message. Popup dialog with language selection will be
    added in follow up commit.
    
    Currently this depends on a96d3e507d82bff977d66f60addc0d127a2ef267,
    otherwise NPE happens in JS RPC code in ChangeScreen2.
    
    Change-Id: Ifc192ed64be761da1517f681340b8afa629cf0a7

---
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/HelloWorldAction.java
@@ -0,0 +1,60 @@
+// Copyright (C) 2013 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.cookbook;
+
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.gerrit.extensions.restapi.RestModifyView;
+import com.google.gerrit.extensions.webui.UiAction;
+import com.google.gerrit.server.CurrentUser;
+import com.google.gerrit.server.IdentifiedUser;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+class HelloWorldAction implements UiAction<RevisionResource>,
+    RestModifyView<RevisionResource, HelloWorldAction.Input> {
+
+  static class Input {
+    String language;
+  }
+
+  private Provider<CurrentUser> cu;
+
+  @Inject
+  HelloWorldAction(Provider<CurrentUser> cu) {
+    this.cu = cu;
+  }
+
+  @Override
+  public Description getDescription(
+      RevisionResource resource) {
+    return new Description()
+        .setLabel("Say hello")
+        .setTitle("Say hello in different languages")
+        .setVisible(cu.get() instanceof IdentifiedUser)
+        .setEnabled(true);
+  }
+
+  @Override
+  public String apply(RevisionResource rev, Input input) {
+    boolean french = !Strings.isNullOrEmpty(input.language)
+        && input.language.equals("french");
+    String greeting = String.format("%s %s!",
+        french ? "Bonjour" : "Hello",
+        Objects.firstNonNull(cu.get().getUserName(), "world"));
+    return greeting;
+  }
+}
