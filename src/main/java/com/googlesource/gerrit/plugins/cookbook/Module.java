683bab1fcdf6b4c567c996df7a9866b9a31d0b90
David Pursehouse <david.pursehouse@sonymobile.com>
2014-09-13 00:32:23 +0200

    Add example of hashtag validation
    Change-Id: I58e9f6e0f3704ba14891cc3c978926e8ff523cf1

--- src/main/java/com/googlesource/gerrit/plugins/cookbook/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/Module.java
@@ -27,6 +27,7 @@ import com.google.gerrit.extensions.webui.BranchWebLink;
 import com.google.gerrit.extensions.webui.PatchSetWebLink;
 import com.google.gerrit.extensions.webui.ProjectWebLink;
 import com.google.gerrit.extensions.webui.TopMenu;
+import com.google.gerrit.server.change.HashtagValidationListener;
 import com.google.gerrit.server.config.ProjectConfigEntry;
 import com.google.gerrit.server.git.validators.MergeValidationListener;
 import com.google.gerrit.server.git.validators.UploadValidationListener;
@@ -60,6 +61,8 @@ public class Module extends AbstractModule {
         .to(DenyUploadExample.class);
     DynamicSet.bind(binder(), MergeValidationListener.class)
         .to(MergeUserValidator.class);
+    DynamicSet.bind(binder(), HashtagValidationListener.class)
+        .to(HashtagValidator.class);
     configurePluginParameters();
   }
 
