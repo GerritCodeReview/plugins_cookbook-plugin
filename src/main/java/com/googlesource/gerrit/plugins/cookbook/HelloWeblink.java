525037a8c5a67c0771b6fba432b446509e9dc99b
Sven Selberg <sven.selberg@sonymobile.com>
2014-09-24 10:47:36 +0200

    Changes to HelloWeblink for Weblink refactoring
    Change-Id: I3da853e7d8a3501cc16b37eca1777d2619c8e387

--- src/main/java/com/googlesource/gerrit/plugins/cookbook/HelloWeblink.java
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/HelloWeblink.java
@@ -14,47 +14,99 @@
 
 package com.googlesource.gerrit.plugins.cookbook;
 
-import com.google.gerrit.extensions.webui.BranchWebLink;
-import com.google.gerrit.extensions.webui.PatchSetWebLink;
-import com.google.gerrit.extensions.webui.ProjectWebLink;
+import com.google.gerrit.extensions.common.WebLinkInfo;
+import com.google.gerrit.extensions.webui.WebLink;
+import com.google.gerrit.server.change.FileResource;
+import com.google.gerrit.server.change.RevisionResource;
+import com.google.gerrit.server.project.BranchResource;
+import com.google.gerrit.server.project.ProjectResource;
 
-public class HelloWeblink implements PatchSetWebLink, ProjectWebLink, BranchWebLink {
-  private String name = "HelloLink";
-  private String placeHolderUrlProject =
+class HelloWeblink {
+  private static String name = "HelloLink";
+  private static String myImageUrl = "http://placehold.it/16x16.gif";
+  private static String placeHolderUrlProject =
       "http://my.hellolink.com/project=%s";
-  private String placeHolderUrlProjectBranch =
+  private static String placeHolderUrlProjectBranch =
       "http://my.hellolink.com/project=%s-branch=%s";
-  private String placeHolderUrlProjectCommit =
+  private static String placeHolderUrlProjectCommit =
       placeHolderUrlProject + "/commit=%s";
-  private String myImageUrl = "http://placehold.it/16x16.gif";
+  private static String placeHolderUrlProjectCommitFile =
+      placeHolderUrlProjectCommit + "/file=%s";
 
-  @Override
-  public String getLinkName() {
-    return name ;
+  private static String getLinkName() {
+    return name;
   }
 
-  @Override
-  public String getPatchSetUrl(String project, String commit) {
-    return String.format(placeHolderUrlProjectCommit, project, commit);
-  }
-
-  @Override
-  public String getProjectUrl(String project) {
-    return String.format(placeHolderUrlProject, project);
-  }
-
-  @Override
-  public String getImageUrl() {
+  private static String getImageUrl() {
     return myImageUrl;
   }
 
-  @Override
-  public String getBranchUrl(String projectName, String branchName) {
-    return String.format(placeHolderUrlProjectBranch, projectName, branchName);
+  private static String getTarget() {
+    return WebLink.Target.BLANK;
   }
 
-  @Override
-  public String getTarget() {
-    return Target.BLANK;
+  public static class PatchSetWebLink implements WebLink<RevisionResource> {
+
+    @Override
+    public WebLinkInfo getWebLinkInfoFor(RevisionResource resource) {
+      return new WebLinkInfo(getLinkName(),
+          getImageUrl(),
+          getPatchSetUrl(resource.getChange().getProject().get(),
+              resource.getPatchSet().getRevision().get()),
+          getTarget());
+    }
+
+    private String getPatchSetUrl(String project, String commit) {
+      return String.format(placeHolderUrlProjectCommit, project, commit);
+    }
+
+  }
+
+  public static class BranchWebLink implements WebLink<BranchResource> {
+
+    @Override
+    public WebLinkInfo getWebLinkInfoFor(BranchResource resource) {
+      return new WebLinkInfo(getLinkName(), getImageUrl(), getBranchUrl(
+          resource.getName(), resource.getBranchKey().get()), getTarget());
+    }
+
+    private String getBranchUrl(String projectName, String branchName) {
+      return String
+          .format(placeHolderUrlProjectBranch, projectName, branchName);
+    }
+
+  }
+
+  public static class ProjectWebLink implements WebLink<ProjectResource> {
+
+    @Override
+    public WebLinkInfo getWebLinkInfoFor(ProjectResource resource) {
+      return new WebLinkInfo(getLinkName(), getImageUrl(),
+          getProjectUrl(resource.getName()), getTarget());
+    }
+
+    private String getProjectUrl(String project) {
+      return String.format(placeHolderUrlProject, project);
+    }
+
+  }
+
+  public static class FileWebLink implements WebLink<FileResource> {
+
+    @Override
+    public WebLinkInfo getWebLinkInfoFor(FileResource resource) {
+      return new WebLinkInfo(getLinkName(),
+          getImageUrl(),
+          getFileUrl(resource.getRevision().getChange().getProject().get(),
+              resource.getRevision().getPatchSet().getRefName(),
+              resource.getPatchKey().getFileName()),
+          getTarget());
+    }
+
+    private String getFileUrl(String projectName, String revision, String fileName) {
+      return String.format(placeHolderUrlProjectCommitFile,
+          projectName, revision, fileName);
+    }
+
   }
 }
