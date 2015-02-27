2e31964a66d3a01821c10b0797f18ce323325cd0
David Pursehouse <david.pursehouse@sonymobile.com>
2015-02-27 15:16:19 +0900

    Add example of TrackingValueExtractor
    Finds and returns as tracking IDs issues that are mentioned in the
    commit message in the format:
    
    COOKBOOK-ISSUE=COOKBOOK-123456
    
    Change-Id: I21d45d857eb40fcb592a0adfe3285dd7490843cc

---
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/TrackingValueExtractorExample.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.cookbook;
+
+import com.google.common.collect.Lists;
+
+import com.google.gerrit.server.config.TrackingValueExtractor;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TrackingValueExtractorExample implements TrackingValueExtractor {
+
+  private final String PATTERN = "^COOKBOOK-ISSUE=(COOKBOOK-[0-9]{6,})$";
+
+  @Override
+  public List<String> getValues(String commitMessage) {
+    List<String> values = Lists.newArrayList();
+    Pattern pattern = Pattern.compile(PATTERN);
+
+    for (String line : commitMessage.split("\n")) {
+      Matcher matcher = pattern.matcher(line);
+      if (matcher.matches()) {
+        values.add(matcher.group(1));
+      }
+    }
+
+    return values;
+  }
+}
