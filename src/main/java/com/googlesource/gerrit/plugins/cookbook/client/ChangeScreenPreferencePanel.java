cd640528d92362b645b02e78ff8cade0f5fae1f0
Edwin Kempin <edwin.kempin@sap.com>
2015-07-16 15:25:54 +0200

    Add example of how to replace change screen with custom screen
    There is a new user preference that allows users to configure the
    default change screen or the cookbook change screen.
    
    Change-Id: Ie29658256b80cff5bd5329601e7f14e1b856cffb
    Signed-off-by: Edwin Kempin <edwin.kempin@sap.com>

---
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/client/ChangeScreenPreferencePanel.java
@@ -0,0 +1,123 @@
+// Copyright (C) 2015 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.cookbook.client;
+
+import com.google.gerrit.client.info.AccountPreferencesInfo;
+import com.google.gerrit.plugin.client.Plugin;
+import com.google.gerrit.plugin.client.extension.Panel;
+import com.google.gerrit.plugin.client.rpc.RestApi;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import java.util.Map;
+
+public class ChangeScreenPreferencePanel extends VerticalPanel {
+  private final static String DEFAULT = "DEFAULT";
+  private final static String COOKBOOK = "COOKBOOK";
+  private final static String OTHER = "OTHER";
+  private final static String DEFAULT_URL_PATH = "/c/";
+  private final static String COOKBOOK_URL_PATH = "/x/" + Plugin.get().getName() +  "/c/";
+
+  static class Factory implements Panel.EntryPoint {
+    @Override
+    public void onLoad(Panel panel) {
+      panel.setWidget(new ChangeScreenPreferencePanel());
+    }
+  }
+
+  ChangeScreenPreferencePanel() {
+    new RestApi("accounts").id("self").view("preferences")
+        .get(new AsyncCallback<AccountPreferencesInfo>() {
+          @Override
+          public void onSuccess(AccountPreferencesInfo result) {
+            display(result);
+          }
+
+          @Override
+          public void onFailure(Throwable caught) {
+            // never invoked
+          }
+        });
+  }
+
+  private void display(final AccountPreferencesInfo info) {
+    HorizontalPanel p = new HorizontalPanel();
+    add(p);
+
+    Label label = new Label("Change Screen:");
+    p.add(label);
+    label.getElement().getStyle().setMarginRight(5, Unit.PX);
+    label.getElement().getStyle().setMarginTop(2, Unit.PX);
+    final ListBox box = new ListBox();
+    p.add(box);
+
+    box.addItem(DEFAULT, DEFAULT);
+    box.addItem(COOKBOOK, COOKBOOK);
+
+    String selected = DEFAULT;
+    if (info.urlAliases().containsKey(DEFAULT_URL_PATH)) {
+      String urlPath = info.urlAliases().get(DEFAULT_URL_PATH);
+      if (urlPath.equals(COOKBOOK_URL_PATH)) {
+        selected = COOKBOOK;
+      } else if (!urlPath.equals(DEFAULT_URL_PATH)) {
+        box.addItem(OTHER, OTHER);
+        selected = OTHER;
+      }
+    }
+
+    for (int i = 0; i < box.getItemCount(); i++) {
+      if (selected.equals(box.getValue(i))) {
+        box.setSelectedIndex(i);
+        break;
+      }
+    }
+
+    box.addChangeHandler(new ChangeHandler() {
+      @Override
+      public void onChange(ChangeEvent event) {
+        if (box.getSelectedValue().equals(OTHER)) {
+          return;
+        }
+
+        Map<String, String> urlAliases = info.urlAliases();
+        if (box.getSelectedValue().equals(COOKBOOK)) {
+          urlAliases.put(DEFAULT_URL_PATH, COOKBOOK_URL_PATH);
+        } else {
+          urlAliases.remove(DEFAULT_URL_PATH);
+        }
+        info.setUrlAliases(urlAliases);
+
+        new RestApi("accounts").id("self").view("preferences")
+            .put(info, new AsyncCallback<AccountPreferencesInfo>() {
+              @Override
+              public void onSuccess(AccountPreferencesInfo result) {
+                Plugin.get().refreshUserPreferences();
+              }
+
+              @Override
+              public void onFailure(Throwable caught) {
+                // never invoked
+              }
+            });
+      }
+    });
+  }
+}
