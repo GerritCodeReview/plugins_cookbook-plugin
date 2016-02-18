0cad8b43f89cb59c44b60564dbebed8e97600584
Craig Chapel <cchapel@codeaurora.org>
2016-02-18 13:01:01 -0700

    SampleOperand: register as sample operand
    The sample search operator operand operates with the "has" operator
    and will find any change that has an even change id.
    
    Change-Id: Ib264c547c3c4eded4de64f3ed73c5215015feb8c

---
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/SampleHasOperand.java
@@ -0,0 +1,65 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.gerrit.plugins;
+
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.reviewdb.server.ReviewDb;
+import com.google.gerrit.server.query.OperatorPredicate;
+import com.google.gerrit.server.query.Predicate;
+import com.google.gerrit.server.query.QueryParseException;
+import com.google.gerrit.server.query.change.ChangeData;
+import com.google.gerrit.server.query.change.ChangeQueryBuilder;
+import com.google.gerrit.server.query.change.ChangeQueryBuilder.ChangeHasOperandFactory;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.Singleton;
+
+@Singleton
+public class SampleHasOperand implements ChangeHasOperandFactory {
+  public static class HasEvenPredicate extends OperatorPredicate<ChangeData> {
+    private final Provider<ReviewDb> db;
+
+    HasEvenPredicate(Provider<ReviewDb> db) {
+      super(ChangeQueryBuilder.FIELD_HAS, "cr");
+      this.db = db;
+    }
+
+    @Override
+    public boolean match(final ChangeData change) throws OrmException {
+      return (change.change(db).getChangeId() % 2 == 0);
+    }
+
+    @Override
+    public int getCost() {
+      return 1;
+    }
+  }
+
+  private final Provider<ReviewDb> dbProvider;
+
+  @Inject
+  public SampleHasOperand(Provider<ReviewDb> dbProvider) {
+    this.dbProvider = dbProvider;
+  }
+
+  @Override
+  public Predicate<ChangeData> create(ChangeQueryBuilder builder)
+      throws QueryParseException {
+    return new HasEvenPredicate(dbProvider);
+  }
+}
+
