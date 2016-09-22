b9bc482e095eef2de4ad66a681331486909d62e6
Gustaf Lundh <gustaflh@axis.com>
2016-09-22 17:33:11 +0200

    AssigneeValidator example
    Will not allow a user to be assigned to more than 5 changes.
    
    Change-Id: I1887f7007e66bd5ccac207c7789be2712ffbd00a

---
+++ src/main/java/com/googlesource/gerrit/plugins/cookbook/AssigneeValidator.java
@@ -0,0 +1,56 @@
+// Copyright (C) 2014 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.cookbook;
+
+import com.google.gerrit.reviewdb.client.Account;
+import com.google.gerrit.reviewdb.client.Change;
+import com.google.gerrit.server.query.QueryParseException;
+import com.google.gerrit.server.query.change.ChangeQueryBuilder;
+import com.google.gerrit.server.query.change.ChangeQueryProcessor;
+import com.google.gerrit.server.validators.AssigneeValidationListener;
+import com.google.gerrit.server.validators.ValidationException;
+import com.google.gwtorm.server.OrmException;
+import com.google.inject.Inject;
+
+import org.eclipse.jgit.util.io.DisabledOutputStream;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Set;
+
+public class AssigneeValidator implements AssigneeValidationListener {
+  private static int MAX_ASSIGNED_CHANGES = 5;
+
+  @Inject
+  ChangeQueryBuilder queryBuilder;
+
+  @Inject
+  ChangeQueryProcessor queryProcessor;
+
+  @Override
+  public void validateAssignee(Change change, Account assignee)
+      throws ValidationException {
+    try {
+      if (queryProcessor
+          .query(queryBuilder.assignee(assignee.getPreferredEmail())).entities()
+          .size() > MAX_ASSIGNED_CHANGES) {
+        throw new ValidationException("Cannot assign user to more than "
+            + MAX_ASSIGNED_CHANGES + " changes");
+      }
+    } catch (OrmException e1) {
+    } catch (QueryParseException e2) {
+    }
+  }
+}
